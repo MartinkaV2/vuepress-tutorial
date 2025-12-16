476e5f5dc212b25efd419beefc37b75393dff5d7
MartinkaV2 <medveczmartin@gmail.com>
2025-12-16 20:45:48 +0100

    build

---
+++ docs/.vuepress/.temp/pages/404.html.js
@@ -0,0 +1,3 @@
+import comp from "C:/Users/Martin/Documents/GitHub/vuepress-tutorial/docs/.vuepress/.temp/pages/404.html.vue"
+const data = JSON.parse("{\"path\":\"/404.html\",\"title\":\"\",\"lang\":\"hu-HU\",\"frontmatter\":{\"layout\":\"NotFound\"},\"headers\":[],\"git\":{},\"filePathRelative\":null}")
+export { comp, data }
